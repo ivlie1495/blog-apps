e5b444e32a285114c4c4d0598c52119e9f29c72a
ivanlie <ivan.lie@meditap.id>
2024-07-18 17:00:27 +0700

    fetch data

--- app/about/projects/page.tsx
+++ app/about/projects/page.tsx
@@ -1,5 +1,24 @@
-const Page = () => {
-	return <div>Project Page</div>
+const getData = async () => {
+	return (await fetch('http://localhost:3001/repos')).json()
+}
+
+const Page = async () => {
+	const data = await getData()
+
+	return (
+		<div className="p-20">
+			<h1 className="mb-8 text-xl">Projects</h1>
+			<ul>
+				{data.map((repo: any) => (
+					<li key={repo.id} className="mb-4">
+						<div>{repo.title}</div>
+						<div>{repo.description}</div>
+						<div>{repo.stargazers_count}</div>
+					</li>
+				))}
+			</ul>
+		</div>
+	)
 }
 
 export default Page
