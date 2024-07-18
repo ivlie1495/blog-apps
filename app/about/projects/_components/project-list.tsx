9728b49f09f0e3d081f794879d4c0e08c95de049
ivanlie <ivan.lie@meditap.id>
2024-07-18 19:57:33 +0700

    project list layout

--- app/about/projects/_components/project-list.tsx
+++ app/about/projects/_components/project-list.tsx
@@ -1,5 +1,14 @@
+import {
+	Card,
+	CardContent,
+	CardDescription,
+	CardHeader,
+	CardTitle,
+} from '@/components/ui/card'
+import { Star } from 'lucide-react'
+
 const getData = async () => {
-	await new Promise((resolve) => setTimeout(resolve, 3000))
+	// await new Promise((resolve) => setTimeout(resolve, 3000))
 
 	return (
 		await fetch('http://localhost:3001/repos', { cache: 'no-store' })
@@ -10,12 +19,23 @@ const ProjectList = async () => {
 	const data = await getData()
 
 	return (
-		<ul>
+		<ul className="grid grid-cols-1 gap-4 md:grid-cols-2">
 			{data.map((repo: any) => (
 				<li key={repo.id} className="mb-4">
-					<div>{repo.title}</div>
-					<div>{repo.description}</div>
-					<div>{repo.stargazers_count}</div>
+					<Card className="h-full">
+						<CardHeader>
+							<div className="flex items-center justify-between">
+								<CardTitle>{repo.title}</CardTitle>
+								<div className="flex items-center gap-2">
+									<Star className="h-4 w-4" />
+									{repo.stargazers_count}
+								</div>
+							</div>
+						</CardHeader>
+						<CardContent>
+							<CardDescription>{repo.description}</CardDescription>
+						</CardContent>
+					</Card>
 				</li>
 			))}
 		</ul>
