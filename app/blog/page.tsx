ee8a41bcd23914be27bbdffc811484f4d893beff
ivanlie <ivan.lie@meditap.id>
2024-07-25 07:25:33 +0700

    post list

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -12,12 +12,17 @@ const Pages = async () => {
 				Stay up to date with my latest blog posts.
 			</div>
 			<ul className="grid grid-cols-1 gap-8 md:grid-cols-2">
-				{posts.map(({ frontmatter, slug }) => (
-					<li key={slug}>
-						<Link href={`/blog/${slug}`} className="text-2xl font-semibold">
-							{frontmatter.title as string}
+				{posts.map((post) => (
+					<li key={post!.slug}>
+						<Link
+							href={`/blog/${post!.slug}`}
+							className="text-2xl font-semibold"
+						>
+							{post!.frontmatter.title as string}
 						</Link>
-						<div className="mt-2 text-sm">{frontmatter.date as string}</div>
+						<div className="mt-2 text-sm">
+							{post!.frontmatter.date as string}
+						</div>
 					</li>
 				))}
 			</ul>
